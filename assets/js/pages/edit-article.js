42ce59bc22350335a04fa7213c46b681070429dc
MariusTBjercke <marius@bjerckemedia.no>
2022-05-13 19:47:24 +0200

    More work on edit articles section

--- assets/js/pages/edit-article.js
+++ assets/js/pages/edit-article.js
@@ -1,3 +1,28 @@
+import {initFroala} from "../functions/froala";
+import {ajaxPostJson} from "../functions/ajax";
+
 up.compiler('.edit-article', function(element) {
+
+    const content = element.querySelector('#content');
+    const submitBtn = element.querySelector('.edit-article__submit');
+
+    // Initialize Froala editor.
+    initFroala(content);
+
+    submitBtn.addEventListener('click', function(e) {
+        const titleInput = element.querySelector('#title');
+        const contentInput = element.querySelector('#content');
+        const articleId = element.querySelector('.edit-article__form').getAttribute('data-article-id');
+        const backgroundCheck = element.querySelector('#background-image-checkbox');
+        const backgroundImage = element.querySelector('#background-image');
+        const background_file = backgroundImage.files[0];
+
+        let formData = new FormData();
+        formData.append('action', 'edit_article');
+        formData.append('title', titleInput.value);
+        formData.append('content', contentInput.value);
+        formData.append('background_check', backgroundCheck.checked);
+        formData.append("background_file", background_file);
+    });
 
 })
